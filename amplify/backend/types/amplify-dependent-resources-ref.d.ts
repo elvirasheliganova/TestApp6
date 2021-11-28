4d34bb05236091388d461e46b20eeeeee7606fa9
Elvira Shelyganova <elvirashelyganova@gmail>
2021-11-28 12:15:30 +0100

    testApp6+ amplyfy + auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "testapp626cf37f0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
